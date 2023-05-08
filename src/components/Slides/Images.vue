608cef4b2bd74461987f7b497bbc8447062ae8c6
Warren Galyen <wgalyen@hotmail.com>
2023-05-08 15:03:02 -0800

    Working on the slide part - Adjust slide values - Working on swipe event - Improvement of slide animation

--- src/components/Slides/Images.vue
+++ src/components/Slides/Images.vue
@@ -2,33 +2,26 @@
   <div
     :class="[
     'slideshow-images',
-    !animationType && `slideshow-images--none`,
-    animationType === 'fade' && `slideshow-images--fade`,
-    animationType === 'horizontal' && `slideshow-images--horizontal`,
-    styleType && `slideshow-images--${styleType}`,
-    state.animated && 'animated',
+    state.computedAnimationTypeClass,
+    imageType && `type--${imageType}`,
+    state.playAnimation && 'play-animation',
   ]"
-    :style="{
-    '--speed-slide-animation': `${duration}ms`,
-    '--image-size': imageSize,
-    '--active-column': state.active,
-  }"
-    @touchstart="onTouchStart"
-    @touchmove="onTouchMove"
-    @touchend="onTouchEnd"
-    @mousedown="onTouchStart"
-    @mousemove="onTouchMove"
-    @mouseup="onTouchEnd">
+    :style="state.computedContainerStyle">
     <div ref="wrap" class="wrap">
+      <figure v-if="state.computedShowFirstItem" class="first">
+        <img v-if="state.loaded[items.length-1]" :src="items[items.length-1].src" :alt="items[items.length-1].title">
+      </figure>
       <figure
         v-for="(item, key) in items"
         :ref="el => { figures[key] = el }"
         :class="[
         (state.active === key && !!state.activeClassName) && state.activeClassName,
-        (state.animate === key && !!state.animateClassName) && state.animateClassName,
+        (state.nextKey === key && !!state.nextClassName) && state.nextClassName,
       ]">
         <img v-if="state.loaded[key]" :src="item.src" :alt="item.title">
-        <LoadingUnit v-else class="loading"/>
+      </figure>
+      <figure v-if="state.computedShowLastItem" class="last">
+        <img v-if="state.loaded[0]" :src="items[0].src" :alt="items[0].title">
       </figure>
     </div>
     <i class="overlay"/>
@@ -37,8 +30,8 @@
 
 <script>
 import { defineComponent, ref, reactive, computed } from 'vue';
-import LoadingUnit from '~/components/Loading/Unit';
 import * as util from '~/libs/util';
+import LoadingUnit from '~/components/Loading/Unit';
 
 export default defineComponent({
   name: 'SlidesImages',
@@ -46,29 +39,63 @@ export default defineComponent({
     LoadingUnit,
   },
   props: {
-    initialActive: { type: Number, default: 0 },
+    items: { type: Array, required: true }, // Unknown load check list
+    initialActive: { type: Number, default: 0 }, // Initial active slide
     animationType: { type: String, default: null }, // null,'fade','horizontal'
-    styleType: { type: String, default: null }, // null,contain,cover
-    items: { type: Array, required: true },
-    duration: { type: Number, default: 800 }, // ms
-    imageSize: { type: String, default: '100%' },
-    loop: { type: Boolean },
+    imageType: { type: String, default: null }, // null,contain,cover
+    duration: { type: Number, default: 800 }, // animation speed(ms)
+    imageSize: { type: Number, default: 100 }, // slide image scale(%)
+    loop: { type: Boolean }, // slide loop
+    movePos: { type: Number, default: undefined },
   },
   setup(props, context)
   {
     let state = reactive({
-      loaded: new Array(props.items.length).fill(false),
-      active: props.initialActive,
+      loaded: new Array(props.items.length).fill(false), // Image load check list
+      active: props.initialActive, // currently active slide number
       activeClassName: 'current',
-      animate: undefined,
-      animateClassName: undefined,
-      animated: false,
+      nextKey: undefined,
+      nextClassName: undefined,
+      playAnimation: false,
+      computedAnimationTypeClass: computed(() => {
+        switch (props.animationType)
+        {
+          case 'fade':
+            return 'animation--fade';
+          case 'horizontal':
+            return 'animation--horizontal';
+          default:
+            return 'animation--none';
+        }
+      }),
+      computedContainerStyle: computed(() => {
+        let result = {
+          '--speed-slide-animation': `${props.duration}ms`,
+          '--image-size': `${props.imageSize}%`,
+        };
+        if (props.animationType === 'horizontal')
+        {
+          result[`--active-column`] = (state.nextKey !== undefined) ? state.nextKey : state.active;
+          if (props.movePos !== undefined)
+          {
+            result['--move-pos'] = `${props.movePos}vw`;
+          }
+        }
+        return result;
+      }),
+      computedShowFirstItem: computed(() => {
+        if (!props.loop) return false;
+        return props.items[props.items.length-1] && props.animationType === 'horizontal';
+      }),
+      computedShowLastItem: computed(() => {
+        if (!props.loop) return false;
+        return props.items[0] && props.animationType === 'horizontal';
+      }),
     });
     let _active = props.initialActive;
     const figures = ref([]);
     const wrap = ref(null);
     let targetElement = null;
-    let move = false;
 
     // set loaded
     state.loaded[props.initialActive] = true;
@@ -98,26 +125,35 @@ export default defineComponent({
       {
         case 'fade':
           context.emit('animation-control', true);
-          state.animated = true;
-          state.activeClassName = 'current ready';
-          state.animate = _active;
-          state.animateClassName = 'next ready';
+          state.playAnimation = true;
+          state.activeClassName = 'fadeout ready';
+          state.nextKey = _active;
+          state.nextClassName = 'fadein ready';
           await util.sleep(20);
-          state.animateClassName = 'next';
+          state.nextClassName = 'fadein';
           targetElement = figures.value[_active];
           targetElement.addEventListener('transitionend', onTransitionEnd);
           break;
         case 'horizontal':
           context.emit('animation-control', true);
-          state.animated = true;
-          state.active = _active;
+          state.playAnimation = true;
+          if (props.loop)
+          {
+            if (state.active === 0 && _active >= props.items.length - 1)
+            {
+              state.nextKey = -1;
+            }
+            else if (state.active >= props.items.length - 1 && _active === 0)
+            {
+              state.nextKey = props.items.length;
+            }
+            state.active = _active;
+          }
+          else
+          {
+            state.active = _active;
+          }
           wrap.value.addEventListener('transitionend', onTransitionEnd);
-          // TODO: The current slide has problems with the first and last slides with `loop=true`.
-          // TODO: A phenomenon that cannot be passed from the last to the first occurs.
-          // TODO: After analyzing the other slides, the last item before the first and the first item at the end are copied and prepared for animation.
-          // TODO: After the animation is over, fix the position value and adjust the last to move to the first and the first to the last.
-          // TODO: The first item is placed with `0-100vw`, the last item with `last+100vw`.
-          // TODO: If it is burdensome to arrange in a state beyond the area, you can cope with it by increasing it as it is and correcting the `key` value to push the order.
           break;
         default:
           state.active = _active;
@@ -129,18 +165,18 @@ export default defineComponent({
       switch (props.animationType)
       {
         case 'fade':
-          state.animate = undefined;
-          state.animateClassName = undefined;
+          state.playAnimation = false;
+          state.nextKey = undefined;
+          state.nextClassName = undefined;
           state.active = _active;
           state.activeClassName = 'current';
           break;
         case 'horizontal':
-          state.active = _active;
-          state.activeClassName = 'current';
+          state.playAnimation = false;
+          state.nextKey = undefined;
           state.loaded = util.setAreaTrue(state.loaded, props.items.length, props.initialActive, props.loop);
           break;
       }
-      state.animated = false;
       context.emit('animation-control', false);
       removeTransitionEndEvent();
     }
@@ -150,33 +186,12 @@ export default defineComponent({
       targetElement.removeEventListener('transitionend', onTransitionEnd);
       targetElement = null;
     }
-    function onTouchStart(e)
-    {
-      if (props.animationType !== 'horizontal') return;
-      move = true;
-      // console.log('onTouchStart');
-    }
-    function onTouchMove(e)
-    {
-      if (!move) return;
-      // console.log('onTouchMove');
-    }
-    function onTouchEnd(e)
-    {
-      if (!move) return;
-      move = false;
-      // console.log('onTouchEnd');
-      // TODO: context.emit('change-active')
-    }
 
     return {
       state,
       figures,
       wrap,
       play,
-      onTouchStart,
-      onTouchMove,
-      onTouchEnd,
     };
   },
   emits: {
