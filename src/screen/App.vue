bfa4f4eebd1f83748adc41103cff306fa2bbf3e1
Warren Galyen <wgalyen@hotmail.com>
2023-05-08 15:03:02 -0800

    Working on the slide part - Adjust slide values - Working on swipe event - Improvement of slide animation

--- src/screen/App.vue
+++ src/screen/App.vue
@@ -4,10 +4,12 @@
 </template>
 
 <script>
-import { defineComponent, reactive, onMounted } from 'vue';
+import { defineComponent, reactive, onMounted, watch } from 'vue';
+import { useStore } from 'vuex';
 import * as util from '~/libs/util';
 import Container from '~/screen/Container';
 import LoadingIntro from '~/components/Loading/Intro';
+
 export default defineComponent({
   name: 'App',
   components: {
@@ -16,38 +18,53 @@ export default defineComponent({
   },
   setup()
   {
+    const store = useStore();
     let state = reactive({
       loading: true,
       dev: process.env.NODE_ENV === 'development',
     });
+
     // method
     function updateTheme()
     {
-      // TODO: Dark mode related part work
+      // TODO: dark mode related part work
       console.log('update theme');
     }
-    async function refresh()
+    function start()
+    {
+      state.loading = false;
+    }
+    function stop()
     {
       state.loading = true;
+    }
+    async function restart()
+    {
+      stop();
       // TODO: I arbitrarily delayed it by 3 seconds, but I need to find a reasonable delay time.
       await util.sleep(3000);
-      state.loading = false;
+      start();
     }
 
     // watch
-    // watch(() => store.state.preference.theme, updateTheme);
+    // watch(() => store.state.preference, () => {
+    //   console.log('updated preference');
+    // });
+
     // lifecycles
     onMounted(async () => {
       await util.sleep(500);
-      // TODO: Restore the values from storage to the vuex area.
+      // TODO: restore the values from storage to the vuex area
       // TODO: Otherwise, json values in the server are retrieved and restored to the vuex area.
       state.loading = false;
     });
+
     return {
       state,
-      refresh,
+      restart,
+      start,
+      stop,
     };
   }
 });
 </script>
-
