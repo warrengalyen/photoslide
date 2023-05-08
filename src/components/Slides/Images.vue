e0104af8b10c9c4a0bf5b5e8523cd74878b82259
Warren Galyen <wgalyen@hotmail.com>
2023-05-08 15:03:02 -0800

    Working on the slide control

--- src/components/Slides/Images.vue
+++ src/components/Slides/Images.vue
@@ -2,13 +2,16 @@
   <div
     :class="[
     'slideshow-images',
+    !animationType && `slideshow-images--none`,
     animationType === 'fade' && `slideshow-images--fade`,
     animationType === 'horizontal' && `slideshow-images--horizontal`,
     styleType && `slideshow-images--${styleType}`,
+    state.animated && 'animated',
   ]"
     :style="{
     '--speed-slide-animation': `${duration}ms`,
     '--image-size': imageSize,
+    '--active-column': state.active,
   }"
     @touchstart="onTouchStart"
     @touchmove="onTouchMove"
@@ -16,22 +19,24 @@
     @mousedown="onTouchStart"
     @mousemove="onTouchMove"
     @mouseup="onTouchEnd">
-    <figure
-      v-for="(item, key) in items"
-      :ref="el => { figures[key] = el }"
-      :class="[
-      (state.activeKey === key && !!state.activeClassName) && state.activeClassName,
-      (state.animateKey === key && !!state.animateClassName) && state.animateClassName,
-    ]">
-      <img v-if="state.loaded[key]" :src="item.src" :alt="item.title">
-      <LoadingUnit v-else class="loading"/>
-    </figure>
+    <div ref="wrap" class="wrap">
+      <figure
+        v-for="(item, key) in items"
+        :ref="el => { figures[key] = el }"
+        :class="[
+        (state.active === key && !!state.activeClassName) && state.activeClassName,
+        (state.animate === key && !!state.animateClassName) && state.animateClassName,
+      ]">
+        <img v-if="state.loaded[key]" :src="item.src" :alt="item.title">
+        <LoadingUnit v-else class="loading"/>
+      </figure>
+    </div>
     <i class="overlay"/>
   </div>
 </template>
 
 <script>
-import { defineComponent, onMounted, ref, watch, reactive } from 'vue';
+import { defineComponent, ref, reactive, computed } from 'vue';
 import LoadingUnit from '~/components/Loading/Unit';
 import * as util from '~/libs/util';
 
@@ -41,68 +46,101 @@ export default defineComponent({
     LoadingUnit,
   },
   props: {
-    animationType: { type: String, default: 'fade' }, // null,'fade','horizontal'
+    initialActive: { type: Number, default: 0 },
+    animationType: { type: String, default: null }, // null,'fade','horizontal'
     styleType: { type: String, default: null }, // null,contain,cover
     items: { type: Array, required: true },
-    active: { type: Number, default: 0 },
     duration: { type: Number, default: 800 }, // ms
     imageSize: { type: String, default: '100%' },
+    loop: { type: Boolean },
   },
   setup(props, context)
   {
     let state = reactive({
       loaded: new Array(props.items.length).fill(false),
-      activeKey: props.active,
+      active: props.initialActive,
       activeClassName: 'current',
-      animateKey: null,
-      animateClassName: null,
+      animate: undefined,
+      animateClassName: undefined,
+      animated: false,
     });
+    let _active = props.initialActive;
     const figures = ref([]);
+    const wrap = ref(null);
     let targetElement = null;
     let move = false;
 
     // set loaded
-    state.loaded[props.active] = true;
+    state.loaded[props.initialActive] = true;
+    if (props.animationType === 'horizontal')
+    {
+      state.loaded = util.setAreaTrue(state.loaded, props.items.length, props.initialActive, props.loop);
+    }
 
     // methods
-    function play()
+    async function play(n = null)
     {
+      if (typeof n !== 'number') return;
       removeTransitionEndEvent();
+      // set temp active
+      _active = Number(n);
+      // init image load event
+      if (!state.loaded[_active])
+      {
+        const image = new Image();
+        image.onload = () => {
+          state.loaded[_active] = true;
+        };
+        image.src = props.items[_active].src;
+      }
+      // play motion
       switch (props.animationType)
       {
         case 'fade':
           context.emit('animation-control', true);
-          state.activeClassName = 'current current-to';
-          state.animateKey = props.active;
-          state.animateClassName = 'from';
-          util.sleep(30).then(() => {
-            state.animateClassName = 'from to';
-            targetElement = figures.value[props.active];
-            targetElement.addEventListener('transitionend', onTransitionEnd);
-          });
+          state.animated = true;
+          state.activeClassName = 'current ready';
+          state.animate = _active;
+          state.animateClassName = 'next ready';
+          await util.sleep(20);
+          state.animateClassName = 'next';
+          targetElement = figures.value[_active];
+          targetElement.addEventListener('transitionend', onTransitionEnd);
           break;
         case 'horizontal':
-          console.log('horizontal slide animation')
+          context.emit('animation-control', true);
+          state.animated = true;
+          state.active = _active;
+          wrap.value.addEventListener('transitionend', onTransitionEnd);
+          // TODO: The current slide has problems with the first and last slides with `loop=true`.
+          // TODO: A phenomenon that cannot be passed from the last to the first occurs.
+          // TODO: After analyzing the other slides, the last item before the first and the first item at the end are copied and prepared for animation.
+          // TODO: After the animation is over, fix the position value and adjust the last to move to the first and the first to the last.
+          // TODO: The first item is placed with `0-100vw`, the last item with `last+100vw`.
+          // TODO: If it is burdensome to arrange in a state beyond the area, you can cope with it by increasing it as it is and correcting the `key` value to push the order.
           break;
         default:
-          state.activeKey = props.active;
+          state.active = _active;
           break;
-      }
-      if (!state.loaded[props.active])
-      {
-        const image = new Image();
-        image.onload = () => {
-          state.loaded[props.active] = true;
-        };
-        image.src = props.items[props.active].src;
       }
     }
     function onTransitionEnd()
     {
-      state.animateKey = undefined;
-      state.animateClassName = undefined;
-      state.activeKey = props.active;
-      state.activeClassName = 'current';
+      switch (props.animationType)
+      {
+        case 'fade':
+          state.animate = undefined;
+          state.animateClassName = undefined;
+          state.active = _active;
+          state.activeClassName = 'current';
+          break;
+        case 'horizontal':
+          state.active = _active;
+          state.activeClassName = 'current';
+          state.loaded = util.setAreaTrue(state.loaded, props.items.length, props.initialActive, props.loop);
+          break;
+      }
+      state.animated = false;
       context.emit('animation-control', false);
       removeTransitionEndEvent();
     }
@@ -112,29 +150,30 @@ export default defineComponent({
       targetElement.removeEventListener('transitionend', onTransitionEnd);
       targetElement = null;
     }
-    function onTouchStart()
+    function onTouchStart(e)
     {
+      if (props.animationType !== 'horizontal') return;
       move = true;
       // console.log('onTouchStart');
     }
-    function onTouchMove()
+    function onTouchMove(e)
     {
       if (!move) return;
       // console.log('onTouchMove');
     }
-    function onTouchEnd()
+    function onTouchEnd(e)
     {
       if (!move) return;
       move = false;
       // console.log('onTouchEnd');
+      // TODO: context.emit('change-active')
     }
-
-    // watches
-    watch(() => props.active, play);
 
     return {
       state,
       figures,
+      wrap,
+      play,
       onTouchStart,
       onTouchMove,
       onTouchEnd,
@@ -142,112 +181,9 @@ export default defineComponent({
   },
   emits: {
     'animation-control': null,
+    'change-active': null,
   },
 });
 </script>
 
-<style lang="scss" scoped>
-.slideshow-images {
-  position: relative;
-  box-sizing: border-box;
-  height: 100vh;
-  user-select: none;
-  figure {
-    display: none;
-    margin: 0;
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    box-sizing: border-box;
-    background-color: var(--color-bg);
-    > img {
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      display: block;
-      transform: translate(-50%, -50%);
-      max-width: var(--image-size, 100%);
-      max-height: var(--image-size, 100%);
-      box-sizing: border-box;
-    }
-    &.current {
-      display: block;
-    }
-  }
-  &--fade {
-    figure {
-      transition-property: opacity;
-      transition-duration: var(--speed-slide-animation, 800ms);
-      transition-timing-function: ease-out;
-      &.from {
-        display: block;
-        z-index: 2;
-        opacity: 0;
-      }
-      &.to {
-        opacity: 1;
-      }
-      &.current {
-        opacity: 1;
-        z-index: 1;
-        &.current-to {
-          opacity: 0;
-        }
-      }
-    }
-  }
-  &--horizontal {
-    figure {
-      transition-property: transform;
-      transition-duration: var(--speed-slide-animation, 800ms);
-      transition-timing-function: ease-out;
-      //&.from {
-      //  display: block;
-      //  transform: translateX(-100%);
-      //}
-      //&.next {
-      //  display: block;
-      //  transform: translateX(100%);
-      //}
-    }
-  }
-  &--contain {
-    figure {
-      > img {
-        width: var(--image-size, 80%);
-        height: var(--image-size, 80%);
-        object-fit: contain;
-      }
-    }
-  }
-  &--cover {
-    figure {
-      > img {
-        width: var(--image-size, 80%);
-        height: var(--image-size, 80%);
-        object-fit: cover;
-      }
-    }
-  }
-  .overlay {
-    display: block;
-    position: absolute;
-    z-index: 5;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    background: transparent;
-  }
-}
-.loading {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  --loading-size: 50px;
-  --loading-speed: 500ms;
-}
-</style>
+<style src="./Images.scss" lang="scss" scoped></style>
