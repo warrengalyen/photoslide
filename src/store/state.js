18a23c3f1afcb0b1c1fb70b793b664b6f32189ea
Warren Galyen <wgalyen@hotmail.com>
2023-05-12 11:12:28 -0700

    Working with thumbnail image lists

--- src/store/state.js
+++ src/store/state.js
@@ -1,23 +1,29 @@
 export default {
   preference: {
     general: {
-      screenColor: 'light', // dark,light,system
+      name: 'Photoslide',
+      description: 'slideshow description',
+      screenColor: 'system', // dark,light,system
       language: 'en',
       hud: true,
       hoverHud: false,
       visibleContents: {
+        menu: true,
         thumbnail: true,
+        caption: true,
+        controller: true,
+        paginate : true ,
       },
     },
     style: {
-      imageScale: 80, // %
+      imageScale: [100,100], // %
       imageType: null, // null,contain,cover
       captionScale: 100, // %
       captionMargin: 24, // px
     },
     slides: {
       initialNumber: 0,
-      animationType: 'horizontal', // null,fade,horizontal
+      animationType: 'fade', // null,none,fade,horizontal
       animationSpeed: 500,
       animationCaptionType: null, // null,shuffle
       autoplay: false,
@@ -28,41 +34,66 @@ export default {
       swipe: true,
     },
     keyboard: {
-      enableShortcut: true,
+      enable: true,
     },
   },
   slides: [
     {
+      src: 'https://www.mechanikadesign.com/data/test-images/photoslide/black-and-silver-balls-001.jpg',
+      thumbnail: null,
+      title: 'small image',
+      description : 'for small images...' ,
+    },
+    {
       src: 'https://www.mechanikadesign.com/data/test-images/photoslide/matrix-001-002.jpg',
+      thumbnail: null,
       title: 'cureeent',
       description: 'current description',
     },
     {
       src: 'https://www.mechanikadesign.com/data/test-images/photoslide/hud-tadpole-001.jpg',
+      thumbnail: null,
       title: 'preeev',
       description: 'prevv sdmig sdigfs dmg\nsdmig sdgolp sdgm,',
     },
     {
       src: 'https://www.mechanikadesign.com/data/test-images/photoslide/double-generator-001.jpg',
+      thumbnail: null,
       title: 'neeext',
       description: 'nexxtt description',
     },
     {
       src: 'https://www.mechanikadesign.com/data/test-images/photoslide/circle-body-001.jpg',
+      thumbnail: null,
       title: 'fioooo',
       description: 'nexxtt description',
     },
     {
       src: 'https://www.mechanikadesign.com/data/test-images/photoslide/noise-color-001.jpg',
+      thumbnail: null,
       title: 'barrr',
       description: 'nexxtt description',
     },
     {
       src: 'https://www.mechanikadesign.com/data/test-images/photoslide/2019-09-18_001.jpg',
+      thumbnail: null,
       title: 'choooo',
       description: 'smdopgmspdg\nsd gjsdgp',
     },
+    {
+      src: 'https://www.mechanikadesign.com/data/test-images/photoslide/triangle-beeple-001.jpg',
+      thumbnail: null,
+      title : 'more than hahabo' ,
+      description: 'sdigg msdkgfp smdgp sdgiosdgi sdmg',
+    },
+    {
+      src: 'https://www.mechanikadesign.com/data/test-images/photoslide/horizon-zero-dawn-1.jpg',
+      thumbnail: null,
+      title: 'Horizon Zero Dawn',
+      description : 'nexxtt description',
+    },
   ],
   mode: null, // null,thumbnail,preference
-  dev: process.env.NODE_ENV === 'development',
+  activeSlide: undefined,
+  dev: process.env.NODE_ENV === 'development', // TODO: You can delete it if you don't use it.
 };
