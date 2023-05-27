6619d08cb91df1b56bd775a0cdedb2019ea0455c
Warren Galyen <wgalyen@hotmail.com>
2023-05-27 10:49:00 -0700

    Adjustment for iPad

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # photoslide
 
-Image slideshow
+The purpose of this app is to flip through photos one by one in the browser full screen.
 
+## Usage
+
+## Develop Usage
+
+## thanks
+
+- vue3: https://v3.vuejs.org
+- example images: https://unsplash.com
 
 <a href="https://www.flaticon.com/free-icon/slideshow_3650928?related_id=3650928" title="slideshow icons">App icon created by Smashicons - Flaticon</a>
--- src/components/Slides/index.vue
+++ src/components/Slides/index.vue
@@ -139,6 +139,7 @@ export default defineComponent({
     function onTouchStart(e)
     {
       e.stopPropagation();
+      if (e.touches && e.touches.length > 1) e.preventDefault();
       if (state.animated) return;
       if (!store.state.preference.slides.swipe) return;
       if (store.state.preference.slides.animationType !== 'horizontal') return;
@@ -188,7 +189,7 @@ export default defineComponent({
       swipeMeta = undefined;
 
       // stop if short enough to click
-      if (elapsedTime < 120 || percent < 5)
+      if (elapsedTime < 60 || percent < 1)
       {
         runAutoplay(true);
         return;
@@ -204,7 +205,7 @@ export default defineComponent({
       else
       {
         // short touch
-        if (percent > 10) action(dir);
+        if (percent > 5) action(dir);
         else cancel();
       }
     }
