507c95551a29b30788d9703d57d9929482facd37
Warren Galyen <wgalyen@hotmail.com>
2023-06-26 21:07:00 -0700

    Data structure change primary task

--- src/store/state.js
+++ src/store/state.js
@@ -4,4 +4,7 @@ import defaults from '~/store/defaults';
 // set state
 let state = convertPureObject(defaults);
 
+// TODO: temporary adjustment for testing
+state.usePreference.data = false;
+
 export default state;
